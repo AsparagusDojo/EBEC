89605d87821e4cddd01eef92905bcc7307a8a81d
AsparagusDojo <112291014+AsparagusDojo@users.noreply.github.com>
2022-10-03 20:41:31 -0400

    Add files via upload

--- prime_numbers_han522.py
+++ prime_numbers_han522.py
@@ -37,7 +37,6 @@ def is_prime(num):
         for i in range(2,num):
             if (num % i) == 0:
                 return False
-                break
             else:
                 return True
     else:
